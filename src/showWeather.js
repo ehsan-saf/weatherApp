572822b807b8a8f927047a50adf9634a771f0aec
root <root@Laptop-Ehsan>
2024-01-25 15:53:52 +0330

    Create box for the info

--- src/showWeather.js
+++ src/showWeather.js
@@ -1,9 +1,11 @@
+const list = document.querySelector(".weather-list");
+
 export default function displayWeather(current) {
-  console.log(current);
-  document.querySelector(".weather-icon").src = getIcon(current);
+  const box = createBox(current);
+  list.appendChild(box);
 }
 
-function getIcon(data) {
+function getIconSrc(data) {
   let time = "";
   const name = data.icon.slice(-7);
   if (data.is_day) {
@@ -11,8 +13,22 @@ function getIcon(data) {
   } else {
     time = "night";
   }
-  const iconUrl = `./icons/${time}/${name}`;
-  return iconUrl;
+  const iconSrc = `./icons/${time}/${name}`;
+  return iconSrc;
 }
 
-function createBox() {}
+function createBox(data) {
+  const box = document.createElement("div");
+  box.classList.add("weather-box");
+
+  const icon = document.createElement("img");
+  icon.src = getIconSrc(data);
+
+  const temp = document.createElement("p");
+  temp.textContent = data.temp_c;
+
+  box.appendChild(icon);
+  box.appendChild(temp);
+
+  return box;
+}
