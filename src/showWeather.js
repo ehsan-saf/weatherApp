747765f22c18026f97e3431279d8fd7147c2ffa2
root <root@Laptop-Ehsan>
2024-01-25 09:31:55 +0330

    Basic style for search

---
+++ src/showWeather.js
@@ -0,0 +1,3 @@
+function displayWeather(current, forecast) {}
+
+function createBox() {}
--- src/weather.js
+++ src/weather.js
@@ -1,16 +1,17 @@
 export default async function getWeather(location) {
-  const data = (await getData(location)).current;
+  const data = await getData(location);
+  const { current } = data;
   return {
-    condition: data.condition.text,
-    icon: data.condition.icon,
-    temp_c: data.temp_c,
-    temp_f: data.temp_f,
-    humidity: data.humidity,
+    condition: current.condition.text,
+    icon: current.condition.icon,
+    temp_c: current.temp_c,
+    temp_f: current.temp_f,
+    humidity: current.humidity,
   };
 }
 
 async function getData(location) {
-  const address = `https://api.weatherapi.com/v1/current.json?key=92af089f4bee44ea93b193454241501&q=${location};
+  const address = `https://api.weatherapi.com/v1/forecast.json?key=92af089f4bee44ea93b193454241501&q=${location};
     `;
   const request = await fetch(address);
   return request.json();
